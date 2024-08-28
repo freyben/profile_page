6666be0cf7acb775bc83c3d3329a521bf2135c35
Freyben-L <derjayman@gmail.com>
2024-08-28 15:33:44 +0200

    Add a comment to python_commands.sh

--- python_commands.sh
+++ python_commands.sh
@@ -2,5 +2,6 @@
 import datetime
 
 date = datetime.datetime.now().strftime( "%d.%m.%Y %H:%M Uhr" )
+# wow incredible!!!!
 print(f"Hello world! :)  \n  Date and time right now are:  {date} ")
 # print(date)
