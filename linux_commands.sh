725c9d7339da41405ae8d961ec64a28abed82072
Freyben-L <derjayman@gmail.com>
2024-08-28 15:05:59 +0200

    Add new features

---
+++ linux_commands.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ls -al
+pwd
+echo "running bash script"
---
+++ python_commands.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import datetime
+
+date = datetime.datetime.now().strftime( "%d.%m.%Y %H:%M Uhr" )
+print(f"Hello world! :)  \n  Date and time right now are:  {date} ")
+# print(date)
