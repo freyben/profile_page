120ba4b676f151046b0a130d250650dc7d9e69ec
Freyben-L <derjayman@gmail.com>
2024-08-28 15:20:05 +0200

    Add comments to testfile.py

--- testfile.py
+++ testfile.py
@@ -1,4 +1,6 @@
 import datetime
 
+# This is a test file to show the datetime command in python
+
 date = datetime.datetime.now().strftime( "%d.%m.%Y %H:%M Uhr" )
 print(f"Hello world! :)  \n  Date and time right now are:  {date} ")
