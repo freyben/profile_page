2e2b61569bb5ab39cd5db9216fb18c5213cd23c3
Freyben-L <derjayman@gmail.com>
2024-08-30 16:02:16 +0200

    Cleanup and commenting

--- script.js
+++ script.js
@@ -1,51 +1,41 @@
 
-// printing to the  console
-
-console.log('Server is running on port 3000');
-
-
-// printing in an alert window
-// window.alert("Server is running on port 3000");
-
-// define variables in javascript with let
-// let variabl_name =  a value;
-
-// define a constant with const
-
-let my_name = "Alice"
-
-console.log("My name is: " + my_name);
-console.log("Who the fuck is " + my_name + "?")
-
-// let myBiography = document.getElementsByClassName(".biography")[0];
-
-var test = document.getElementsByTagName("h")
-console.log(test);
-
-// let myBiography = document.getElementsByClassName("biography")[0];
-// console.log(myBiography.innerHTML);
-
-// myBiography.innerHTML = "Hello my paragraph element has changed!";
-
 var myBody = document.body;
 
 
 
 
 function Darkmode(){
-    // myBody.style.backgroundColor = "#36454F"; //Charcoal
-
-    myBody.classList.toggle("dark-mode");
-    
-    
-    // var button = document.querySelector(#button-dark-mode);
-    // button.addEventListener('click', () => {
-    //     button.style.backgroundColor = '#39ca74';
-    // });
-
-
-
-
+    myBody.classList.toggle("dark-mode");    
 }
 
-// Darkmode();
+const myImage=document.querySelector("#profile_pic")
+var switch_1 = 0
+
+function FirstImage(repeats = 1) {
+
+    if (switch_1 === 0) {
+        myImage.src="kermit_mirror.jpg"
+        switch_1 = 1
+    }
+    else {
+        myImage.src="kermit.jpg"
+        switch_1 = 0
+    }
+    // myBody.classList.toggle("dark-mode");
+}
+let myInterval
+function Madness() {
+    // FirstImage()
+    alert("What do you mean by madness?");
+    
+    myInterval = setInterval(FirstImage, 50);
+    
+    }
+
+function Stop_Madness() {
+    if (myInterval != undefined) {
+    clearInterval(myInterval);
+    }
+    else
+        alert("There is no madness right now!")
+}
