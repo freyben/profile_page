df1d301bec57047712484bb275d585e19d384133
Freyben-L <derjayman@gmail.com>
2024-08-30 13:15:44 +0200

    Add dark-theme

---
+++ script.js
@@ -0,0 +1,51 @@
+
+// printing to the  console
+
+console.log('Server is running on port 3000');
+
+
+// printing in an alert window
+// window.alert("Server is running on port 3000");
+
+// define variables in javascript with let
+// let variabl_name =  a value;
+
+// define a constant with const
+
+let my_name = "Alice"
+
+console.log("My name is: " + my_name);
+console.log("Who the fuck is " + my_name + "?")
+
+// let myBiography = document.getElementsByClassName(".biography")[0];
+
+var test = document.getElementsByTagName("h")
+console.log(test);
+
+// let myBiography = document.getElementsByClassName("biography")[0];
+// console.log(myBiography.innerHTML);
+
+// myBiography.innerHTML = "Hello my paragraph element has changed!";
+
+var myBody = document.body;
+
+
+
+
+function Darkmode(){
+    // myBody.style.backgroundColor = "#36454F"; //Charcoal
+
+    myBody.classList.toggle("dark-mode");
+    
+    
+    // var button = document.querySelector(#button-dark-mode);
+    // button.addEventListener('click', () => {
+    //     button.style.backgroundColor = '#39ca74';
+    // });
+
+
+
+
+}
+
+// Darkmode();
